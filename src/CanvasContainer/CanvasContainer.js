d028a1418fd40086f632d4701039773741c52d65
“Danny <developer@solinal.org>
2024-03-05 13:05:37 -0500

    ready for development

--- src/CanvasContainer/CanvasContainer.js
+++ src/CanvasContainer/CanvasContainer.js
@@ -10,62 +10,101 @@ export default function CanvasContainer() {
     const fabricCanvas = new fabric.Canvas('canvas', {
       width: 800,
       height: 600,
+      selection: true,
+      lockScalingFlip: true,
     });
+
+
+    let initialTop, initialLeft, initialScaleX, initialScaleY;
+    fabricCanvas.on('mouse:down', function (event) {
+      const obj = event.target;
+      if (obj && obj.selectable) {
+        initialTop = obj.top;
+        initialLeft = obj.left;
+        initialScaleX = obj.scaleX;
+        initialScaleY = obj.scaleY;
+      }
+    });
+
+    // fabricCanvas.on('selection:created', function(event){
+    //   const obj = fabricCanvas.getActiveObject();
+    //   if (obj) {
+    //     if (obj.type === 'activeSelection') {
+    //       const center = obj.getCenterPoint();
+    //       console.log("top - left" , obj.top, obj.left)
+    //       console.log("centerY - centerX", center.y, center.x)
+    //       obj._objects.forEach(innerObj => {
+    //         innerObj.top = (innerObj.top + center.y) * obj.scaleY;
+    //         innerObj.left = (innerObj.left + center.x) * obj.scaleX;
+    //         innerObj.scaleX = innerObj.scaleX * obj.scaleX;
+    //         innerObj.scaleY = innerObj.scaleY * obj.scaleY;
+    //         console.log("innertop - innerleft" , innerObj.top, innerObj.left , innerObj)
+    //       });
+    //     }
+    //   }
+    // })
+
+    fabricCanvas.on('object:scaling', function (e) {
+      const obj = e.target;
+      obj.lockScalingFlip = true;
+
+    });
+
+
+    fabricCanvas.on('before:selection:cleared',function(e){
+      const modifiedObject = e.target;
+      if (modifiedObject._objects && modifiedObject.type === 'activeSelection' && modifiedObject.isOnScreen()) {
+        const group = modifiedObject;
+        const center = group.getCenterPoint();
+        group._objects.forEach(function (innerObj) {
+
+          innerObj.top = (innerObj.top + center.y) * group.scaleY;
+          innerObj.left = (innerObj.left + center.x) * group.scaleX;
+          innerObj.scaleX = innerObj.scaleX * group.scaleX;
+          innerObj.scaleY = innerObj.scaleY * group.scaleY;
+
+          console.log("inner", innerObj.left, innerObj.top, innerObj.scaleX, innerObj.scaleY);
+          if (!innerObj.isOnScreen()) {
+            console.log("A group object is outside the screen!!");
+          }
+          
+        });
+      } 
+    })
+
+    fabricCanvas.on('object:modified', function (e) {
+      const modifiedObject = e.target;
+      console.log("modified")
+      if (modifiedObject) {
+          if (!modifiedObject.isOnScreen()) {
+            console.log("objeto fuera")
+            modifiedObject.set({
+              left: initialLeft,
+              top: initialTop,
+              scaleX: initialScaleX,
+              scaleY: initialScaleY,
+            });
+            modifiedObject.setCoords();
+            fabricCanvas.requestRenderAll();
+          }
+        }
+      });
 
     var rect = new fabric.Rect({ fill: "red", width: 100, height: 100 });
     var rect2 = new fabric.Rect({ fill: "blue", width: 100, height: 100 });
     fabricCanvas.add(rect);
     fabricCanvas.add(rect2);
 
-    let initialTop, initialLeft;
-
-    fabricCanvas.on('mouse:down', function (event) {
-      const obj = event.target;
-
-      // Guardar las coordenadas iniciales solo si es un objeto seleccionable
-      if (obj && obj.selectable) {
-        initialTop = obj.top;
-        initialLeft = obj.left;
-      }
-    });
-
-    fabricCanvas.on('mouse:up', function (event) {
-      const obj = event.target;
-
-      // Verificar si el objeto es nulo antes de intentar acceder a sus propiedades
-      if (obj && obj.selectable) {
-        // Obtener las coordenadas del objeto al soltarlo
-        const left = obj.left;
-        const top = obj.top;
-        const bottom = top + obj.height * obj.scaleY;
-        const right = left + obj.width * obj.scaleX;
-        // Verificar si el objeto está completamente fuera del canvas
-        if (
-          right < 0 ||  // está fuera del canvas por la parte izquierda
-          left > fabricCanvas.width || // está fuera del canvas por la parte derecha
-          top > fabricCanvas.height ||  // está fuera del canvas por la parte inferior
-          bottom < 0  // está fuera del canvas por la parte de arriba
-        ) {
-          console.log("está fuera");
-          // Restaurar la posición inicial solo si es un objeto seleccionable
-          obj.set({ left: initialLeft, top: initialTop });
-          obj.setCoords();
-          fabricCanvas.requestRenderAll();
-        } else {
-          console.log("está dentro");
-        }
-      }
-    });
-
-
     setCanvas(fabricCanvas);
 
     return () => {
+      fabricCanvas.off('mouse:down');
+      fabricCanvas.off('object:scaling');
+      fabricCanvas.off('object:modified');
+      fabricCanvas.off('selection:created');
       fabricCanvas.dispose();
     };
   }, []);
-
-
 
   const handleImageUpload = (imageUrl) => {
     if (canvas) {
@@ -93,7 +132,6 @@ export default function CanvasContainer() {
     }
   };
 
-
   return (
     <div>
       <SideBar onImageUpload={handleImageUpload} generateDownload={generateDownload} />
@@ -101,3 +139,4 @@ export default function CanvasContainer() {
     </div>
   );
 }
+
