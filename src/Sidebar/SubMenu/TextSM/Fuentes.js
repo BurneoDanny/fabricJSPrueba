ceaf815de82ab3c34b833c3b3f01830b0207e528
jecanart <jecanart@espol.edu.ec>
2024-03-05 16:50:35 -0500

    Remake of text color function

--- src/Sidebar/SubMenu/TextSM/Fuentes.js
+++ src/Sidebar/SubMenu/TextSM/Fuentes.js
@@ -1,46 +1,34 @@
 import { useState } from "react"
+import Circle from '@uiw/react-color-circle';
+
+
 
 export default function Fuentes(props){
 
-  const [fuente, setFuente] = useState("color")
 
-  const changeFont=()=>{
-    fuente = 'font'
-  }
+  const [hex, setHex] = useState('#000000');
+
+
+
 
   
   const changeColor = (cValue) =>{
     if(props.canvas.getActiveObject()){
     props.canvas.getActiveObject().set({fill: cValue})
     props.canvas.renderAll()
-    setFuente(cValue)
+    //setFuente(cValue)
     }
 }
 
   return(
-    <button
-      class="relative group transition-all duration-200 focus:overflow-visible w-max h-max p-2 overflow-hidden flex flex-row items-center justify-center bg-white gap-2 rounded-lg border border-zinc-200"
-      onChange={()=>console.log('change')}>
-      <span>
-        {fuente}
-      </span>
-      <svg class="rotate-90 group-focus:rotate-180" xmlns="http://www.w3.org/2000/svg" width="22" height="22"
-        viewBox="0 0 24 24">
-        <path fill="currentColor"
-          d="m12 10.8l-3.9 3.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z" />
-      </svg>
-      <div
-        class="absolute shadow-lg -bottom-40 left-0 w-full h-max p-2 bg-white border border-zinc-200 rounded-lg flex flex-col gap-2">
-        <span class="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg" onClick={()=>changeColor('red')}>
-          <p >Rojo</p>
-        </span>
-        <span class="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg"onClick={()=>changeColor('blue')}>
-          <p>Azul</p>
-        </span>
-        <span class="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg"onClick={()=>changeColor('green')}>
-          <p>Verde</p>
-        </span>
-      </div>
-    </button>
+    <Circle
+      colors={[ '#000000', '#F44E3B', '#FE9200', '#FCDC00', '#3f51b5' ]}
+      color={hex}
+      onChange={(color) => {
+        setHex(color.hex);
+      }}
+      onClick={changeColor(hex)}
+    />
+
   );
 }
